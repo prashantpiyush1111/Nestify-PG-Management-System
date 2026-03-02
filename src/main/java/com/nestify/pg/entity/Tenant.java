2ca799d1e8b742721feaa9163d236f28dfc48e5b
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-03-03 00:13:24 +0530

    Day 3: Added Tenant Entity (Basic Java Version)

---
+++ src/main/java/com/nestify/pg/entity/Tenant.java
@@ -0,0 +1,45 @@
+package com.nestify.pg.entity;
+
+public class Tenant {
+
+    private Long id;
+    private String name;
+    private String phoneNumber;
+    private String idProof;
+    private String assignedRoomNumber;
+
+    public Tenant() {
+    }
+
+    public Tenant(Long id, String name, String phoneNumber, String idProof, String assignedRoomNumber) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.idProof = idProof;
+        this.assignedRoomNumber = assignedRoomNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getIdProof() {
+        return idProof;
+    }
+
+    public String getAssignedRoomNumber() {
+        return assignedRoomNumber;
+    }
+
+    public void setAssignedRoomNumber(String assignedRoomNumber) {
+        this.assignedRoomNumber = assignedRoomNumber;
+    }
+}
