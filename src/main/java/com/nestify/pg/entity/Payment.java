ed7ff63871fcaaeef381a0f3cf91eed2c28f38f1
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-03-07 21:20:02 +0530

    Day 6: Added Payment Entity for rent tracking

---
+++ src/main/java/com/nestify/pg/entity/Payment.java
@@ -0,0 +1,34 @@
+package com.nestify.pg.entity;
+
+public class Payment {
+
+    private Long id;
+    private String tenantName;
+    private String roomNumber;
+    private double amount;
+    private String paymentDate;
+
+    public Payment(Long id, String tenantName, String roomNumber, double amount, String paymentDate) {
+        this.id = id;
+        this.tenantName = tenantName;
+        this.roomNumber = roomNumber;
+        this.amount = amount;
+        this.paymentDate = paymentDate;
+    }
+
+    public String getTenantName() {
+        return tenantName;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+}
