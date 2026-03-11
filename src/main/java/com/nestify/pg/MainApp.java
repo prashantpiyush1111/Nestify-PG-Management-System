1b864c58ec9f3db46dc7644d93983df7245c7779
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-03-11 20:03:28 +0530

    Day 8: Updated MainApp to test Room, Tenant and Payment services

--- src/main/java/com/nestify/pg/MainApp.java
+++ src/main/java/com/nestify/pg/MainApp.java
@@ -2,8 +2,10 @@ package com.nestify.pg;
 
 import com.nestify.pg.entity.Room;
 import com.nestify.pg.entity.Tenant;
+import com.nestify.pg.entity.Payment;
 import com.nestify.pg.service.RoomService;
 import com.nestify.pg.service.TenantService;
+import com.nestify.pg.service.PaymentService;
 
 public class MainApp {
 
@@ -11,6 +13,7 @@ public class MainApp {
 
         RoomService roomService = new RoomService();
         TenantService tenantService = new TenantService();
+        PaymentService paymentService = new PaymentService();
 
         Room room = new Room(1L, "101", "Single", 5000, true);
         roomService.addRoom(room);
@@ -18,7 +21,11 @@ public class MainApp {
         Tenant tenant = new Tenant(1L, "Rahul", "9876543210", "Aadhar", "101");
         tenantService.addTenant(tenant);
 
+        Payment payment = new Payment(1L, "Rahul", "101", 5000, "10 March");
+        paymentService.addPayment(payment);
+
         System.out.println("Room Added: " + room.getRoomNumber());
         System.out.println("Tenant Added: " + tenant.getName());
+        System.out.println("Payment Done by: " + payment.getTenantName());
     }
 }
