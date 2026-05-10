2799dcb72474bc670c3bbb78974380fcc8f936d2
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-05-11 01:32:28 +0530

    Removed unnecessary setRoom() and getRoom() alias methods to avoid duplicate room handling confusion

--- src/main/java/com/nestify/pg/entity/Tenant.java
+++ src/main/java/com/nestify/pg/entity/Tenant.java
@@ -2,57 +2,44 @@ package com.nestify.pg.entity;
 
 public class Tenant {
 
-    private Long id;
-    private String name;
-    private String phoneNumber;
-    private String idProof;
-    private String assignedRoomNumber;
+	private Long id;
+	private String name;
+	private String phoneNumber;
+	private String idProof;
+	private String assignedRoomNumber;
 
-    public Tenant() {
-    }
+	public Tenant() {
+	}
 
-    public Tenant(Long id, String name, String phoneNumber, String idProof, String assignedRoomNumber) {
-        this.id = id;
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.idProof = idProof;
-        this.assignedRoomNumber = assignedRoomNumber;
-    }
+	public Tenant(Long id, String name, String phoneNumber, String idProof, String assignedRoomNumber) {
+		this.id = id;
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.idProof = idProof;
+		this.assignedRoomNumber = assignedRoomNumber;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+	public String getIdProof() {
+		return idProof;
+	}
 
-    public String getIdProof() {
-        return idProof;
-    }
+	public String getAssignedRoomNumber() {
+		return assignedRoomNumber;
+	}
 
-    public String getAssignedRoomNumber() {
-        return assignedRoomNumber;
-    }
-
-    public void setAssignedRoomNumber(String assignedRoomNumber) {
-        this.assignedRoomNumber = assignedRoomNumber;
-    }
-
-    //  fix for service compatibility
-    public void setRoom(String roomNumber) {
-        this.assignedRoomNumber = roomNumber;
-    }
-
-    public String getRoom() {
-        return this.assignedRoomNumber;
-    }
+	public void setAssignedRoomNumber(String assignedRoomNumber) {
+		this.assignedRoomNumber = assignedRoomNumber;
+	}
 }
