de83442b394f7d40aea9f1d7e57b428f3c502717
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-05-30 15:30:16 +0530

    feat: add Spring Boot main entry point for Nestify PG Management application

---
+++ src/main/java/com/nestify/pg/NestifyApplication.java
@@ -0,0 +1,11 @@
+package com.nestify.pg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestifyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NestifyApplication.class, args);
+    }
+}
